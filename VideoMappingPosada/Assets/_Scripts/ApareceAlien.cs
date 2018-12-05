3fdb6c44270e7ca36a160c3d80900b9cfbad8ed0
Vladimir Totolhua <vtotolhua@gmail.com>
2018-12-04 19:16:35 -0600

    AnimacionAlien
    Ya incluye animacion Alien

---
+++ VideoMappingPosada/Assets/_Scripts/ApareceAlien.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApareceAlien : MonoBehaviour {
+    private Animator estados;
+    // Use this for initialization
+	void Start () {
+        estados = gameObject.GetComponentInParent<Animator>();
+        gameObject.GetComponent<SkinnedMeshRenderer>().enabled = false;
+        StartCoroutine(ActivoAlien(3.5f));
+        StartCoroutine(ActivoAnimacion(7.0f));
+    }
+
+    // Update is called once per fra
+    IEnumerator ActivoAlien(float retardo) {
+        yield return new WaitForSeconds(retardo);
+        gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+    }
+
+    IEnumerator ActivoAnimacion(float TAnimacion) {
+        yield return new WaitForSeconds(TAnimacion);
+        Debug.Log("Hola");
+        estados.SetBool("ataque", false);
+        yield return new WaitForSeconds(5.0f);
+        Debug.Log("Hola 3 ");
+        estados.SetBool("ataque", true);
+        StartCoroutine(ActivoAnimacion(5.0f));
+    }
+    
+}
---
+++ VideoMappingPosada/Assets/_Scripts/Aparece_Ladrillo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aparece_Ladrillo : MonoBehaviour {
+
+    // Use this for initialization
+	void Start () {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        StartCoroutine(CorutinaLadrillo(Random.Range(0.0f, 3.0f)));
+	}
+
+    IEnumerator CorutinaLadrillo (float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+}
